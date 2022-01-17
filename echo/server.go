0ce0af7689633e2e27f4d81c8accdb4731b43af6
Kanan Rahimov <mail@kenanbek.me>
2022-01-17 23:02:13 +0100

    echo initial version

---
+++ echo/server.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+)
+
+type ServerError struct {
+	Message string `json:"message"`
+}
+
+type User struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	Bio      string `json:"bio"`
+}
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.GET("/users/:id", getUser)
+	// e.POST("/users", saveUser)
+	// e.PUT("/users/:id", updateUser)
+	// e.DELETE("/users/:id", deleteUser)
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+// e.GET("/users/:id", getUser)
+func getUser(c echo.Context) error {
+	id := c.Param("id")
+
+	userID, err := strconv.Atoi(id)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, ServerError{"incorrect user ID"})
+	}
+
+	user := User{
+		ID:       userID,
+		Username: fmt.Sprintf("username-%s", id),
+		Bio:      fmt.Sprintf("bio-%s", id),
+	}
+
+	return c.JSON(http.StatusOK, user)
+}

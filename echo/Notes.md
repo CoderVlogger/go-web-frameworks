4d94f16342a02e20a13f920e128484a79012e780
Kanan Rahimov <mail@kenanbek.me>
2022-02-09 22:31:31 +0100

    refactor routers

--- echo/Notes.md
+++ echo/Notes.md
@@ -3,3 +3,19 @@ Open topics:
 - How to use model without adding tags?
 - How to redirect with custom HTTP Method (defaults to "POST")?
 - Org url structure when BE API and FE served from the same app
+- Refactor API urls (maybe group them)
+
+
+API:
+
+    /api
+
+Static / Assets:
+
+    /static
+
+Front-end / Website:
+
+    /
+
+
--- echo/server.go
+++ echo/server.go
@@ -1,126 +1,43 @@
 package main
 
 import (
-	"net/http"
-	"strconv"
-
 	"github.com/CoderVlogger/go-web-frameworks/pkg"
 
 	"github.com/labstack/echo/v4"
 )
 
 var (
-	pageSize                           = 4
-	entityStorage pkg.EntityRepository = pkg.NewEntityMemoryRepository()
+	pageSize  = 4
+	apiPrefix = "/api/"
 )
 
+type WebApplication struct {
+	echoApp          *echo.Echo
+	entityRepository pkg.EntityRepository
+}
+
+func NewWebApplication() *WebApplication {
+	er := pkg.NewEntityMemoryRepository()
+	er.Init()
+
+	return &WebApplication{
+		echoApp:          echo.New(),
+		entityRepository: er,
+	}
+}
+
+func (wa *WebApplication) Init() {
+	wa.initAPIRoutes()
+	wa.initWebsite()
+}
+
+func (wa *WebApplication) Start(address string) {
+	wa.echoApp.Logger.Fatal(wa.echoApp.Start(address))
+}
+
 func main() {
-	app := echo.New()
-	entityStorage.Init()
+	wa := NewWebApplication()
 
-	app.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, Echo!")
-	})
-
-	app.Static("/static", "assets")
-	app.File("/website", "assets/pages/index.html")
-	app.POST("/website", formSubmitEntity)
-
-	app.POST("/entities", addEntity)
-	app.PUT("/entities", updateEntity)
-	app.GET("/entities", listEntities)
-	app.GET("/entities/:id", getEntity)
-	app.DELETE("/entities/:id", deleteEntity)
-
-	app.Logger.Fatal(app.Start(":8080"))
-}
-
-func formSubmitEntity(ctx echo.Context) error {
-	entity := pkg.Entity{}
-
-	if err := ctx.Bind(&entity); err != nil {
-		return ctx.JSON(http.StatusBadRequest, pkg.TextResponse{Message: err.Error()})
-	}
-
-	if err := entityStorage.Add(&entity); err != nil {
-		return ctx.JSON(http.StatusInternalServerError, pkg.TextResponse{Message: err.Error()})
-	}
-
-	// TODO: What's the convention when we do form submit.
-	// return ctx.Redirect(http.StatusTemporaryRedirect, "/website")
-	return ctx.File("assets/pages/index.html")
-}
-
-func addEntity(ctx echo.Context) error {
-	entity := pkg.Entity{}
-
-	if err := ctx.Bind(&entity); err != nil {
-		return ctx.JSON(http.StatusBadRequest, pkg.TextResponse{Message: err.Error()})
-	}
-
-	if err := entityStorage.Add(&entity); err != nil {
-		return ctx.JSON(http.StatusInternalServerError, pkg.TextResponse{Message: err.Error()})
-	}
-
-	return ctx.JSON(http.StatusCreated, entity)
-}
-
-func updateEntity(ctx echo.Context) error {
-	entity := pkg.Entity{}
-
-	if err := ctx.Bind(&entity); err != nil {
-		return ctx.JSON(http.StatusBadRequest, pkg.TextResponse{Message: err.Error()})
-	}
-
-	if err := entityStorage.Update(&entity); err != nil {
-		return ctx.JSON(http.StatusInternalServerError, pkg.TextResponse{Message: err.Error()})
-	}
-
-	return ctx.JSON(http.StatusOK, entity)
-}
-
-func getEntity(ctx echo.Context) error {
-	entityID := ctx.Param("id")
-
-	entity, err := entityStorage.Get(entityID)
-	if err != nil {
-		return ctx.JSON(http.StatusNotFound, pkg.TextResponse{Message: err.Error()})
-	}
-
-	if entity == nil {
-		return ctx.JSON(http.StatusNotFound, pkg.TextResponse{Message: "entity not found"})
-	}
-
-	return ctx.JSON(http.StatusOK, entity)
-}
-
-func listEntities(ctx echo.Context) error {
-	var err error
-
-	page := 1
-
-	pageStr := ctx.QueryParam("page")
-	if pageStr != "" {
-		page, err = strconv.Atoi(pageStr)
-		if err != nil {
-			return ctx.JSON(http.StatusBadRequest, pkg.TextResponse{Message: err.Error()})
-		}
-	}
-
-	entities, err := entityStorage.List(page, pageSize)
-	if err != nil {
-		return ctx.JSON(http.StatusBadRequest, pkg.TextResponse{Message: err.Error()})
-	}
-
-	return ctx.JSON(http.StatusOK, entities)
-}
-
-func deleteEntity(ctx echo.Context) error {
-	entityID := ctx.Param("id")
-
-	if err := entityStorage.Delete(entityID); err != nil {
-		return ctx.JSON(http.StatusInternalServerError, pkg.TextResponse{Message: err.Error()})
-	}
-
-	return ctx.JSON(http.StatusOK, pkg.TextResponse{Message: "entity deleted"})
+	wa.Init()
+	wa.Start(":8080")
 }
